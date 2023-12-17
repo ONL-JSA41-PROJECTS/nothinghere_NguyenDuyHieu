2db424867c8fb68e9621afa7a16a546629caeaae
duyhieu12890 <duyhieu12890@gmail.com>
2023-12-17 11:47:23 +0700

    Change everything

--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,8 @@
+let data_n = undefined;
 (async function() {
     const response = await fetch("./data.json");
     const data = await response.json();
     console.log(data);
+    // localStorage.setItem("data", data);
+    data_n = data;
 })();
