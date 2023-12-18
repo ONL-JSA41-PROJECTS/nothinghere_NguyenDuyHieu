6ff752da444b399372de93dec6e1f662e8858f08
duyhieu12890 <duyhieu12890@gmail.com>
2023-12-18 22:32:22 +0700

    Add render Items

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,48 @@
 let data_n = undefined;
-(async function() {
+async function requestReponse() {
     const response = await fetch("./data.json");
     const data = await response.json();
     console.log(data);
     // localStorage.setItem("data", data);
     data_n = data;
-})();
+    for (let items of data.winKey) {
+        console.log(items);
+        let item = document.createElement("div");
+        item.classList.add("item");
+
+        let imgItems = document.createElement("div");
+        imgItems.classList.add("image-items");
+
+        let image = document.createElement("img");
+        image.setAttribute("src", items.image);
+        imgItems.appendChild(image);
+        item.appendChild(imgItems);
+
+        let contentItem = document.createElement("div");
+        contentItem.classList.add("contentItem");
+
+        let name = document.createElement("h5");
+        name.classList.add("product-name");
+        name.innerText = items.name.toUpperCase();
+        contentItem.appendChild(name);
+
+        let price = document.createElement("h6");
+        price.innerHTML = "<b>Price:</b> " + items.price;
+        contentItem.appendChild(price);
+
+        let btn = document.createElement("button");
+        btn.setAttribute("onclick", "addToCart()");
+        btn.innerHTML = "Buy and to cart"
+        contentItem.appendChild(btn);
+
+        item.appendChild(contentItem);
+        document.getElementsByClassName("showContent")[0].appendChild(item);
+        console.log(items);
+    }
+}
+
+function showCategory(keyWord) {
+    requestReponse();
+}
+
+requestReponse();
