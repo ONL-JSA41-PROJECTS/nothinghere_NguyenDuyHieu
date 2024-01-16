f396fe80e20c40b64e42ee6fff22a8359a8f42d3
duyhieu12890 <duyhieu12890@gmail.com>
2024-01-16 15:44:37 +0700

    Fix access login.after.html when account is not available

---
+++ js/login.fix.js
@@ -0,0 +1,10 @@
+function startLoad() {
+    if (localStorage.getItem('status') == 'undefined' ||
+        localStorage.getItem('status') == 'undefined' ||
+        localStorage.getItem('userName') == 'undefined' ||
+        localStorage.getItem('email') == 'undefined' ||
+        localStorage.getItem('pass') == 'undefined') {
+            localStorage.status = false;
+        }
+    checkStatus();
+}
